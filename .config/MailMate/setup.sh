33add2e1b24fc34e6e6695c3233c59cf08698702
Jesse Claven <jesse.claven@gmail.com>
2019-01-29 11:19:40 +0100

    MailMate: Use a better path for the custom key bindings. Swap the notifications reply button for a delete button.

--- .config/MailMate/setup.sh
+++ .config/MailMate/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-cp ./Contents/Resources/KeyBindings/User.plist /Applications/MailMate.app/Contents/Resources/KeyBindings
+cp ~/.config/MailMate/Contents/Resources/KeyBindings/User.plist /Applications/MailMate.app/Contents/Resources/KeyBindings
+defaults write com.freron.MailMate MmNotificationDeleteButtonEnabled -bool YES
