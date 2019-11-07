ba3ea1034f7a0c67020a0dd6cce28d1e38b43a89
Jesse Claven <jesse.claven@gmail.com>
2019-11-07 00:12:45 +0000

    fish: initial config.

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+fzf_key_bindings
+
+set --universal fish_greeting ""
+
+# dots -------------------------------------------------------------------------
+alias dots='git --git-dir=$HOME/.dots.git/ --work-tree=$HOME'
+
+# Visual -----------------------------------------------------------------------
+
+alias cls='clear && printf "\e[3J"'
+
+export CLICOLOR=1
