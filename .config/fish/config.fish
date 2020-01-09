9fc0f45bc93fc7f1a95a6b1ff1c9a9d0b2227554
Jesse Claven <jesse@duffel.com>
2020-01-09 22:26:23 +0000

    fish: add some useful aliases and cleanup a little

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,3 +10,28 @@ alias dots='git --git-dir=$HOME/.dots.git/ --work-tree=$HOME'
 alias cls='clear && printf "\e[3J"'
 
 export CLICOLOR=1
+
+# Applications -----------------------------------------------------------------
+
+set -g fish_user_paths "/usr/local/opt/terraform@0.11/bin" $fish_user_paths
+
+# ripgrep
+alias rg='rg --smart-case --pretty'
+
+# Git
+alias cdg='cd (git rev-parse --show-toplevel)'
+
+# Emacs
+export EMACS_USER_DIRECTORY=~/.emacs.d
+
+# Homebrew
+export HOMEBREW_NO_ANALYTICS=1
+
+# asdf
+source ~/.asdf/asdf.fish
+
+# Local ------------------------------------------------------------------------
+
+if test -e ~/local.sh
+  source ~/local.sh
+end
