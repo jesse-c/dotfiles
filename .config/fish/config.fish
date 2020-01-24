20a451ba79fe75449921d88e0696b0b707b0def4
Jesse Claven <jesse@duffel.com>
2020-01-24 21:25:38 +0000

    fish: disable ripgrep alias that was causing problems in nvim and add the GPG TTY

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,8 +15,10 @@ export CLICOLOR=1
 
 set -g fish_user_paths "/usr/local/opt/terraform@0.11/bin" $fish_user_paths
 
+export GPG_TTY=(tty)
+
 # ripgrep
-alias rg='rg --smart-case --pretty'
+# alias rg='rg --smart-case --pretty'
 
 # Git
 alias cdg='cd (git rev-parse --show-toplevel)'
