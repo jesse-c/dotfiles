5cee6b03456bbabbe4418ed3ad29f8c9cdc3384a
Jesse Claven <jesse@duffel.com>
2020-03-02 14:00:51 +0000

    fish: Add a prompt symbol for if there's a PLAN file. Add some color variables

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -63,5 +63,11 @@ function fish_prompt --description 'Write out the prompt'
         set prompt_status ' ' (set_color $fish_color_status) "[$last_status]" "$normal"
     end
 
-    echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_vcs_prompt) $normal $prompt_status $suffix " "
+    set -l plan_exists
+    if test -e .plan
+      # Alternate symbols: ✺ 
+      set plan_exists '◎ '
+    end
+
+    echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) (set_color FF1FEF) $plan_exists $normal (__fish_vcs_prompt) $normal $prompt_status $suffix " "
 end
