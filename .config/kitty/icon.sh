48db9625af7f64eb097f46aec8717693396130f8
Jesse Claven <jesse@duffel.com>
2020-03-02 14:01:17 +0000

    kitty: Remove unnecessary command

--- .config/kitty/icon.sh
+++ .config/kitty/icon.sh
@@ -3,4 +3,3 @@
 # https://superuser.com/questions/618501/changing-an-applications-icon-from-the-terminal-osx
 
 cp ./dracula.icns /Applications/kitty.app/Contents/Resources/kitty.icns
-touch /Applications/kitty.app
