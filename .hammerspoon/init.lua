62a310b3a0b46fd03b8e33c130ae084a8c0cae28
Jesse Claven <jesse.claven@me.com>
2020-07-05 20:30:29 +0100

    hammerspoon: disable Mail menu bar and add mouse screen switching

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -56,7 +56,41 @@ end
 wifiWatcher = hs.wifi.watcher.new(ssidChangedCallback)
 wifiWatcher:start()
 
--- AutoVolume ------------------------------------------------------------------
+-- Move mouse cursor ----------------------------------------------------------
+
+local next = 0
+local prev = 1
+
+local mouseMoveMods = {'cmd', 'ctrl'}
+
+local function moveMouseToScreen(direction)
+  local screen = hs.mouse.getCurrentScreen()
+
+  local destinationScreen
+  if direction == next then
+    destinationScreen = screen:next()
+  else
+    destinationScreen = screen:previous()
+  end
+
+  local rect = destinationScreen:fullFrame()
+  local center = hs.geometry.rectMidPoint(rect)
+
+  hs.mouse.setAbsolutePosition(center)
+end
+
+local function moveMouseToNextScreen()
+  moveMouseToScreen(next)
+end
+
+local function moveMouseToPrevScreen()
+  moveMouseToScreen(prev)
+end
+
+hs.hotkey.bind(mouseMoveMods, 'h', moveMouseToPrevScreen)
+hs.hotkey.bind(mouseMoveMods, 'l', moveMouseToNextScreen)
+
+-- AutoVolume -----------------------------------------------------------------
 
 function sleepWatch(eventType)
   if (eventType == hs.caffeinate.watcher.systemWillSleep or eventType == hs.caffeinate.watcher.systemDidWake) then
@@ -106,69 +140,69 @@ hs.hotkey.bind('alt', 'tab', 'Next window', function() switcher:next() end)
 hs.hotkey.bind('alt-shift', 'tab', 'Prev window',function() switcher:previous() end)
 
 -- Mail ------------------------------------------------------------------------
-mail = hs.image.imageFromPath("~/.hammerspoon/bin/mail.png")
-mail:setSize({w=16,h=16})
-mailMenu = hs.menubar.new()
-mailMenu:setIcon(mail)
+-- mail = hs.image.imageFromPath("~/.hammerspoon/bin/mail.png")
+-- mail:setSize({w=16,h=16})
+-- mailMenu = hs.menubar.new()
+-- mailMenu:setIcon(mail)
 
-function updateMailMenu()
-  local succeeded, result, desc = hs.osascript.applescript("tell application \"Mail\" to get the unread count of inbox")
+-- function updateMailMenu()
+--   local succeeded, result, desc = hs.osascript.applescript("tell application \"Mail\" to get the unread count of inbox")
 
-  if (succeeded and result ~= nil) then
-    local count = tostring(result)
-    mailMenu:setTitle(count)
-  end
+--   if (succeeded and result ~= nil) then
+--     local count = tostring(result)
+--     mailMenu:setTitle(count)
+--   end
 
-  local getUnreadList = [[
-    tell application "Mail"
-      set msgs to (get the messages 1 thru 10 of inbox)
-      set unreadMsgsIDs to {}
+--   local getUnreadList = [[
+--     tell application "Mail"
+--       set msgs to (get the messages 1 thru 10 of inbox)
+--       set unreadMsgsIDs to {}
       
-      repeat with msg in msgs
-        if read status of msg is false then
-          set end of unreadMsgsIDs to (get id of msg)
-        end if
-      end repeat
-    end tell
+--       repeat with msg in msgs
+--         if read status of msg is false then
+--           set end of unreadMsgsIDs to (get id of msg)
+--         end if
+--       end repeat
+--     end tell
 
-    return unreadMsgsIDs
-  ]]
-  local succeeded, result, desc = hs.osascript.applescript(getUnreadList)
+--     return unreadMsgsIDs
+--   ]]
+--   local succeeded, result, desc = hs.osascript.applescript(getUnreadList)
 
-  if (succeeded and result ~= nil) then
-    local subjects = {}
+--   if (succeeded and result ~= nil) then
+--     local subjects = {}
 
-    for _,id in pairs(result) do
-      local readFn = function(id) 
-        return function (keys, item)
-          -- TODO Do something after run
-          local _, _, _ = hs.osascript.applescript("tell application \"Mail\" to set read status of (first message of inbox whose id is equal to " .. id .. ") to true")
-        end
-      end
+--     for _,id in pairs(result) do
+--       local readFn = function(id) 
+--         return function (keys, item)
+--           -- TODO Do something after run
+--           local _, _, _ = hs.osascript.applescript("tell application \"Mail\" to set read status of (first message of inbox whose id is equal to " .. id .. ") to true")
+--         end
+--       end
 
-      local deleteFn = function(id)
-        return function (keys, item)
-          -- TODO Do something after run
-          local _, _, _ = hs.osascript.applescript("tell application \"Mail\" to delete (first message of inbox whose id is equal to " .. id .. ")")
-        end
-      end
+--       local deleteFn = function(id)
+--         return function (keys, item)
+--           -- TODO Do something after run
+--           local _, _, _ = hs.osascript.applescript("tell application \"Mail\" to delete (first message of inbox whose id is equal to " .. id .. ")")
+--         end
+--       end
       
-      local succeededSubject, subject, _ = hs.osascript.applescript("tell application \"Mail\" to get subject of (first message of inbox whose id is equal to " .. id .. ")")
-      local succeededSender, sender, _ = hs.osascript.applescript("tell application \"Mail\" to get sender of (first message of inbox whose id is equal to " .. id .. ")")
-      -- TODO Date sent
+--       local succeededSubject, subject, _ = hs.osascript.applescript("tell application \"Mail\" to get subject of (first message of inbox whose id is equal to " .. id .. ")")
+--       local succeededSender, sender, _ = hs.osascript.applescript("tell application \"Mail\" to get sender of (first message of inbox whose id is equal to " .. id .. ")")
+--       -- TODO Date sent
 
-      if (succeededSubject and subject ~= nil and succeededSender and sender ~= nil) then
-        local msgMenu = {}
-        table.insert(msgMenu, {title = "Delete", fn = deleteFn(id)})
-        table.insert(msgMenu, {title = "Mark as read", fn = readFn(id)})
+--       if (succeededSubject and subject ~= nil and succeededSender and sender ~= nil) then
+--         local msgMenu = {}
+--         table.insert(msgMenu, {title = "Delete", fn = deleteFn(id)})
+--         table.insert(msgMenu, {title = "Mark as read", fn = readFn(id)})
 
-        table.insert(subjects, {title = sender .. " — " .. subject, menu = msgMenu})
-      end
-    end
+--         table.insert(subjects, {title = sender .. " — " .. subject, menu = msgMenu})
+--       end
+--     end
 
-    mailMenu:setMenu(subjects)
-  end
-end
+--     mailMenu:setMenu(subjects)
+--   end
+-- end
 
-updateMailMenu()
-hs.timer.doEvery(1, updateMailMenu)
+-- updateMailMenu()
+-- hs.timer.doEvery(1, updateMailMenu)
