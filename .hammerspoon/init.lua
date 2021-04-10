cb9e607f89068a5c6242fbc63398720c3c65daef
Jesse Claven <jesse.claven@me.com>
2021-04-10 16:45:58 +0100

    hammerspoon: Add hyper key

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -56,29 +56,72 @@ end
 wifiWatcher = hs.wifi.watcher.new(ssidChangedCallback)
 wifiWatcher:start()
 
--- Quick application switching -------------------------------------------------
+-- Hyper -----------------------------------------------------------------------
 
--- https://liuhao.im/english/2017/06/02/macos-automation-and-shortcuts-with-hammerspoon.html
+-- https://gist.github.com/ttscoff/cce98a711b5476166792d5e6f1ac5907
 
-local function open(name)
-    return function()
-        hs.application.launchOrFocus(name)
-        if name == 'Finder' then
-            hs.appfinder.appFromName(name):activate()
-        end
-    end
+-- A global variable for the Hyper Mode
+-- Creates a sort of namespace
+k = hs.hotkey.modal.new({}, "F17")
+
+-- Trigger existing hyper key shortcuts
+-- Example:
+-- k:bind({}, 'm', nil, function() hs.eventtap.keyStroke({"cmd","alt","shift","ctrl"}, 'm') end)
+
+-- Build your own
+launch = function(appname)
+  hs.application.launchOrFocus(appname)
+  if name == 'Finder' then
+      hs.appfinder.appFromName(name):activate()
+  end
+  k.triggered = true
 end
 
-hs.hotkey.bind({"alt"}, "t", open("/Applications/Things3.app"))
-hs.hotkey.bind({"alt"}, "k", open("kitty"))
-hs.hotkey.bind({"alt"}, "f", open("Firefox Nightly"))
-hs.hotkey.bind({"alt"}, "m", open("MailMate"))
-hs.hotkey.bind({"alt"}, "a", open("Safari"))
-hs.hotkey.bind({"alt"}, "p", open("Spotify"))
-hs.hotkey.bind({"alt"}, "w", open("Paw"))
-hs.hotkey.bind({"alt"}, "z", open("zoom.us"))
-hs.hotkey.bind({"alt"}, "s", open("Slack"))
-hs.hotkey.bind({"alt"}, "n", open("Notion"))
+-- Single keybinding for app launch
+singleapps = {
+  {'t', '/Applications/Things3.app'},
+  {'k', 'kitty'},
+  {'f', 'Firefox Nightly'},
+  {'m', 'MailMate'},
+  {'a', 'Safari'},
+  {'p', 'Spotify'},
+  {'w', 'Paw'},
+  {'z', 'zoom.us'},
+  {'s', 'Slack'},
+  {'n', 'Notion'},
+  {'r', 'Raycast'},
+  {'d', 'Dash'},
+}
+
+for i, app in ipairs(singleapps) do
+  k:bind({}, app[1], function() launch(app[2]); k:exit(); end)
+end
+
+-- Shortcut to reload config
+ofun = function()
+  hs.reload()
+  hs.alert.show("Config loaded")
+  k.triggered = true
+end
+k:bind({}, 'o', nil, ofun)
+
+-- Enter Hyper Mode when F18 (Hyper/Capslock) is pressed
+pressedF18 = function()
+  k.triggered = false
+  k:enter()
+end
+
+-- Leave Hyper Mode when F18 (Hyper/Capslock) is pressed, send ESCAPE if no
+-- other keys are pressed.
+releasedF18 = function()
+  k:exit()
+  if not k.triggered then
+    hs.eventtap.keyStroke({}, 'ESCAPE')
+  end
+end
+
+-- Bind the Hyper key
+f18 = hs.hotkey.bind({}, 'F18', pressedF18, releasedF18)
 
 -- Move mouse cursor ----------------------------------------------------------
 
@@ -167,7 +210,7 @@ end
 -- DISABLED hs.timer.doEvery(1, updateSpotifyMenu)
 
 -- Window management -----------------------------------------------------------
--- TODO Replace Spectacle
+-- TODO Replace Rectangle
 
 -- Window Switcher
 switcher = hs.window.switcher.new(
