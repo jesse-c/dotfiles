ad328beae18cb89457353785faae0913e9cd61ba
Jesse Claven <jesse.claven@me.com>
2021-04-13 20:28:00 +0100

    neovim: Cleanup abbreviations

--- .config/nvim/lua/settings/init.lua
+++ .config/nvim/lua/settings/init.lua
@@ -4,6 +4,8 @@ local bo = vim.bo -- For the buffer local options
 
 local configuration_folder = vim.fn.expand'~'..'/.config/nvim'
 
+require('abbrs')
+
 -- ====================
 -- Editor
 -- ====================
@@ -51,21 +53,6 @@ if f~=nil then
   io.close(f)
   vim.cmd [[ source ~/.config/nvim/local.vim ]]
 end
------------------
--- Common mistakes
------------------
-vim.cmd [[ cnoreabbrev W! w! ]]
-vim.cmd [[ cnoreabbrev Q! q! ]]
-vim.cmd [[ cnoreabbrev Qall! qall! ]]
-vim.cmd [[ cnoreabbrev Wq wq ]]
-vim.cmd [[ cnoreabbrev Wa wa ]]
-vim.cmd [[ cnoreabbrev wQ wq ]]
-vim.cmd [[ cnoreabbrev W w ]]
-vim.cmd [[ cnoreabbrev Q q ]]
-vim.cmd [[ cnoreabbrev Sp sp ]]
-vim.cmd [[ cnoreabbrev sP sp ]]
-vim.cmd [[ cnoreabbrev Qall qall ]]
-vim.cmd [[ cnoreabbrev FOrmat Format ]]
 ----------------
 -- Miscellaneous
 ----------------
