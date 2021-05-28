82f59001cd898ae224fd216a4deff775cdb926c3
Jesse Claven <jesse.claven@me.com>
2021-05-28 23:27:17 +0100

    fish: Add up function

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -43,6 +43,40 @@ direnv hook fish | source
 # zoxide
 zoxide init fish | source
 
+# https://gist.github.com/zlksnk/81a4993be410586c038f8b3fc140b1c7
+function up
+  set -l dir_to_be_child $argv[1]
+
+  if [ "$dir_to_be_child" = "" ]
+    cd ..
+  else
+    cd -
+    set -l alt_dir $PWD
+    cd -
+    set -l curr_dir $PWD
+
+    set -l is_found 0
+    while [ "$PWD" != "/" ]; and test $is_found -eq 0
+      cd ..
+      set -l res (find $dir_to_be_child -maxdepth 0 -type d 2>/dev/null | wc -l)
+      if test $res -eq 1
+        set is_found 1
+      end
+    end
+
+    if [ "$PWD" = "/" ]
+      cd $alt_dir
+      cd $curr_dir
+      echo "Couldn't find $dir_to_be_child"
+      return 1
+    end
+
+    set -l found_dir $PWD
+    cd $curr_dir
+    cd $found_dir
+  end
+end
+
 # Local ------------------------------------------------------------------------
 
 if test -e ~/local.sh
