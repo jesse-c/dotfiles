04c4fd42437483a17e5b2a849ec3610b49ba9c3b
Jesse Claven <jesse@duffel.com>
2021-08-18 19:14:01 +0100

    neovim: Move mappings to mappings file

--- .config/nvim/lua/mappings/init.lua
+++ .config/nvim/lua/mappings/init.lua
@@ -1,1 +1,8 @@
 vim.o.timeoutlen = 500
+
+vim.cmd [[ nmap <silent> <C-k> :wincmd k<CR> ]]
+vim.cmd [[ nmap <silent> <C-j> :wincmd j<CR> ]]
+vim.cmd [[ nmap <silent> <C-h> :wincmd h<CR> ]]
+vim.cmd [[ nmap <silent> <C-l> :wincmd l<CR> ]]
+
+vim.cmd [[ nmap <silent> <C-q> :q<CR> ]]
--- .config/nvim/lua/ui/init.lua
+++ .config/nvim/lua/ui/init.lua
@@ -40,11 +40,6 @@ autocmd {
 }
 o.splitbelow = true
 o.splitright = true
-vim.cmd [[ nmap <silent> <C-k> :wincmd k<CR> ]]
-vim.cmd [[ nmap <silent> <C-j> :wincmd j<CR> ]]
-vim.cmd [[ nmap <silent> <C-h> :wincmd h<CR> ]]
-vim.cmd [[ nmap <silent> <C-l> :wincmd l<CR> ]]
-vim.cmd [[ nmap <silent> <C-q> :q<CR> ]]
 ----------
 -- Buffers
 ----------
