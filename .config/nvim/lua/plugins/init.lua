fe48b15d4527ab9d10e05eeccd70fad9993e1c9c
Jesse Claven <jesse.claven@me.com>
2021-12-11 12:28:39 +0000

    neovim: Switch to FZF native for Telescope

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -237,13 +237,24 @@ return require("packer").startup(function()
 	-- UI
 	use({
 		"nvim-telescope/telescope.nvim",
-		requires = "nvim-telescope/telescope-fzy-native.nvim",
+		requires = "nvim-telescope/telescope-fzf-native.nvim",
 		config = function()
-			require("telescope").setup({})
-			require("telescope").load_extension("fzy_native")
+			require("telescope").setup({
+				extensions = {
+					fzf = {
+						fuzzy = true, -- false will only do exact matching
+						override_generic_sorter = true, -- override the generic sorter
+						override_file_sorter = true, -- override the file sorter
+						case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+						-- the default case_mode is "smart_case"
+					},
+				},
+			})
+			require("telescope").load_extension("fzf")
 		end,
 	})
-	use("nvim-telescope/telescope-fzy-native.nvim")
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+
 	use({
 		"folke/todo-comments.nvim",
 		config = function()
