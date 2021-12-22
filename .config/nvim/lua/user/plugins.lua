f932a7a868c420ee524b1f1ea5cb747b6c138eb1
Jesse Claven <jesse.claven@me.com>
2021-12-22 20:12:58 +0000

    neovim: Add Tree-sitter playground

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -366,8 +366,31 @@ return packer.startup(function(use)
 						enable = true,
 					},
 				},
+				playground = {
+					enable = true,
+					disable = {},
+					updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+					persist_queries = false, -- Whether the query persists across vim sessions
+					keybindings = {
+						toggle_query_editor = "o",
+						toggle_hl_groups = "i",
+						toggle_injected_languages = "t",
+						toggle_anonymous_nodes = "a",
+						toggle_language_display = "I",
+						focus_language = "f",
+						unfocus_language = "F",
+						update = "R",
+						goto_node = "<cr>",
+						show_help = "?",
+					},
+				},
 			})
 		end,
+	})
+	use({
+		"nvim-treesitter/playground",
+		requires = "nvim-treesitter/nvim-treesitter",
+		event = "BufRead",
 	})
 	use({
 		"nvim-treesitter/nvim-treesitter-textobjects",
