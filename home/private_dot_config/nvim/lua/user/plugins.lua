64ba46389fc1e7a14f37152945c4e14ef23f510d
Jesse Claven <jesse.claven@me.com>
2022-01-14 00:05:20 +0000

    neovim: Add Obsession status to statusline

--- home/private_dot_config/nvim/lua/user/plugins.lua
+++ home/private_dot_config/nvim/lua/user/plugins.lua
@@ -242,7 +242,7 @@ return packer.startup(function(use)
 					lualine_a = { "mode" },
 					lualine_b = { "branch", "diff", { "diagnostics", sources = { "nvim_diagnostic" } } },
 					lualine_c = { condensed_path },
-					lualine_x = { "encoding", "fileformat", "filetype" },
+					lualine_x = { "%{ObsessionStatus('●', '○')}", "encoding", "fileformat", "filetype" },
 					lualine_y = { "progress" },
 					lualine_z = { "location" },
 				},
