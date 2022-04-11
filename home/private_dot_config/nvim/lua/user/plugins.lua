583ef88192e31bd36667ac15d142494c0abb9740
Jesse Claven <jesse.claven@me.com>
2022-04-11 18:53:50 +0100

    neovim: Use new recommended setting

--- home/private_dot_config/nvim/lua/user/plugins.lua
+++ home/private_dot_config/nvim/lua/user/plugins.lua
@@ -405,7 +405,7 @@ return packer.startup(function(use)
 		run = ":TSUpdate",
 		config = function()
 			require("nvim-treesitter.configs").setup({
-				ensure_installed = "maintained",
+				ensure_installed = "all",
 				ignore_install = {},
 				highlight = {
 					enable = true, -- False will disable the whole extension
