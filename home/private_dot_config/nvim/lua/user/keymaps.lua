278c79f950fcb4cba16c3e586b01aa59e7c74d8d
Jesse Claven <jesse.claven@me.com>
2022-05-16 23:23:46 +0100

    neovim: Add command for Packer

--- home/private_dot_config/nvim/lua/user/keymaps.lua
+++ home/private_dot_config/nvim/lua/user/keymaps.lua
@@ -11,3 +11,18 @@ vim.api.nvim_set_keymap("n", "<C-q>", ":q<CR>", { silent = true, noremap = true 
 vim.api.nvim_set_keymap("n", "<C-a>", ":bd<CR>", { silent = true, noremap = true })
 
 vim.api.nvim_set_keymap("n", "<Leader>.", ':lua require("user.a")()<CR>', { silent = true, noremap = true })
+
+-- https://www.reddit.com/r/neovim/comments/um3epn/what_are_your_prizedfavorite_lua_functions/
+local async = require("plenary.async")
+local packer_sync = function()
+	async.run(function()
+		vim.notify.async("Syncing packer.", "info", {
+			title = "Packer",
+		})
+	end)
+	local snap_shot_time = os.date("!%Y-%m-%dT%TZ")
+	vim.cmd("PackerSnapshot " .. snap_shot_time)
+	vim.cmd("PackerSync")
+end
+
+vim.api.nvim_set_keymap("n", "<Leader>ps", "", { callback = packer_sync })
