22960d99de1592fca6163d76f8c42d5d577b8111
Jesse Claven <jesse.claven@me.com>
2022-06-14 11:10:22 +0100

    feat(neovim): Add multi-cursor

--- home/private_dot_config/nvim/lua/user/plugins.lua
+++ home/private_dot_config/nvim/lua/user/plugins.lua
@@ -90,6 +90,48 @@ return packer.startup(function(use)
 			require("nvim-web-devicons").setup({})
 		end,
 	})
+	-- Multiple cursors plugin for vim/neovim
+	use({
+		"mg979/vim-visual-multi",
+		-- event = "BufRead",
+		config = function()
+			-- For some reason, this is only working within vim.cmd
+
+			-- Test 1
+			-- local mappings = {}
+			-- mappings["Add Cursor Down"] = "<S-Down>"
+			-- mappings["Add Cursor Up"] = "<S-Up>"
+			-- mappings["Undo"] = "u"
+			-- mappings["Redo"] = "<C-r>"
+			--
+			-- vim.g.VM_default_mappings = 0
+			-- vim.g.VM_mouse_mappings = 0
+			-- vim.g.VM_maps = mappings
+
+			-- Test 2
+			-- vim.g.VM_mouse_mappings = 0
+
+			-- vim.g.VM_maps = {
+			-- 	["Add Cursor Down"] = "<Down>",
+			-- 	["Add Cursor Up"] = "<Up>",
+			-- 	Undo = "u",
+			-- 	Redo = "<C-r>",
+			-- 	Exit = "<C-c>",
+			-- }
+
+			-- Test 3
+			vim.cmd([[
+			  let g:VM_default_mappings = 0
+			  let g:VM_mouse_mappings = 0
+			  let g:VM_maps = {}
+			  let g:VM_maps["Add Cursor Down"] = '<S-Down>'
+			  let g:VM_maps["Add Cursor Up"] = '<S-Up>'
+			  let g:VM_maps["Undo"] = 'u'
+			  let g:VM_maps["Redo"] = '<C-r>'
+			  let g:VM_maps["Exit"] = '<C-c>'
+			]])
+		end,
+	})
 	-- Rainbow Parentheses Improved, shorter code, no level limit, smooth and fast, powerful configuration
 	use({
 		"luochen1990/rainbow",
