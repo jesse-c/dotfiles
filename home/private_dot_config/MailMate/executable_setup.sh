5bebbd7800db9ee42887bd84acc18d1676bf251d
Jesse Claven <jesse.claven@me.com>
2022-07-06 18:44:02 +0100

    mailmate: Update setup and add more preferences

--- home/private_dot_config/MailMate/executable_setup.sh
+++ home/private_dot_config/MailMate/executable_setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-cp ~/.config/MailMate/Contents/Resources/KeyBindings/User.plist /Applications/MailMate.app/Contents/Resources/KeyBindings
+mkdir -p ~/Library/Application\ Support/MailMate/Resources/KeyBindings/
+
+cp ~/.config/MailMate/KeyBindings/User.plist ~/Library/Application\ Support/MailMate/Resources/KeyBindings/
+
+# https://manual.mailmate-app.com/hidden_preferences
+
 defaults write com.freron.MailMate MmNotificationDeleteButtonEnabled -bool YES
-# defaults write com.freron.MailMate MmMessagesOutlineMoveStrategy -string "none"
+
+defaults write com.freron.MailMate MmMessagesOutlineMoveStrategy -string "none"
+
+defaults write com.freron.MailMate MmAutomaticallyExpandThreadsEnabled -bool YES
+defaults write com.freron.MailMate MmAutomaticallyExpandOnlyWhenCounted -bool YES
