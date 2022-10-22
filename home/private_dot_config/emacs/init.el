668080a61406c2e9818c42f32a9746f90ec784e1
Jesse Claven <jesse.claven@me.com>
2022-10-22 22:57:33 +0100

    emacs: Clojure deps.edn updates

--- home/private_dot_config/emacs/init.el
+++ home/private_dot_config/emacs/init.el
@@ -529,8 +529,21 @@
 
 ;; Clojure
 (use-package clojure-mode)
+(use-package clojure-snippets)
 
-(use-package cider)
+(use-package cider
+  :defer t
+  :config
+  (setq nrepl-log-messages t)
+  (flycheck-clojure-setup)) ;; run setup *after* cider load
+
+(use-package flycheck-clojure
+  :defer t
+  :commands (flycheck-clojure-setup) ;; autoload
+  :config
+  (eval-after-load 'flycheck
+    '(setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages))
+  (add-hook 'after-init-hook #'global-flycheck-mode))
 
 ;; .env
 (use-package dotenv-mode)
