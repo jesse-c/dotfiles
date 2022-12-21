321fb5fd958ad48ff6b6059fd9b8eadc2c22c248
Jesse Claven <jesse.claven@me.com>
2022-12-21 17:26:11 +0000

    emacs: Configure more LSPs

--- home/private_dot_config/emacs/init.el
+++ home/private_dot_config/emacs/init.el
@@ -411,11 +411,70 @@
 (use-package eglot
   ;; It's built-in, so don't load it from an external source
   :ensure nil
+  ;; See: https://github.com/neovim/nvim-lspconfig/tree/89a19315ef4064a144b3d7d1c9a7eefd0e91e51b/lua/lspconfig/server_configurations
   :config
   (add-to-list 'eglot-server-programs
               `(toml-mode "taplo" "lsp" "stdio"))
   (add-to-list 'eglot-server-programs
-              `(toml-ts-mode "taplo" "lsp" "stdio")))
+              `(elixir-mode "elixir-ls"))
+  (add-to-list 'eglot-server-programs
+              `(xml-mode "lemminx"))
+  (add-to-list 'eglot-server-programs
+              `(go-mode "gopls"))
+  (add-to-list 'eglot-server-programs
+              `(bash-mode "bash-language-server" "start"))
+  (add-to-list 'eglot-server-programs
+              `(clojure-mode "clojure-lsp"))
+  (add-to-list 'eglot-server-programs
+              `(latex-mode "ltex-ls"))
+  (add-to-list 'eglot-server-programs
+              `(lua-mode "lua-language-server"))
+  (add-to-list 'eglot-server-programs
+              `(markdown-mode "prosemd-lsp" "--stdio"))
+  (add-to-list 'eglot-server-programs
+              `(python-mode "pylsp"))
+  (add-to-list 'eglot-server-programs
+              `(rust-mode "rust-analyzer"))
+  ;; And/Or Sorbet for Ruby?
+  (add-to-list 'eglot-server-programs
+              `(ruby-mode "solargraph" "stdio"))
+  (add-to-list 'eglot-server-programs
+              `(sql-mode "sqls"))
+  (add-to-list 'eglot-server-programs
+              `(typescript-mode "typescript-language-server" "--stdio"))
+  (add-to-list 'eglot-server-programs
+              `(html-mode "vscode-html-language-server" "--stdio"))
+  (add-to-list 'eglot-server-programs
+              `(web-mode "vscode-html-language-server" "--stdio"))
+  (add-to-list 'eglot-server-programs
+              `(css-mode "vscode-cs-language-server" "--stdio"))
+  (add-to-list 'eglot-server-programs
+              `(json-mode "vscode-json-language-server" "--stdio"))
+  (add-to-list 'eglot-server-programs
+              `(yaml-mode "yaml-language-server" "--stdio"))
+  (add-to-list 'eglot-server-programs
+              `(terraform-mode "terraform-ls" "serve"))
+  :hook
+  (toml-mode-hook . 'eglot-ensure)
+  (elixir-mode-hook . 'eglot-ensure)
+  (xml-mode-hook . 'eglot-ensure)
+  (go-mode-hook . 'eglot-ensure)
+  (bash-mode-hook . 'eglot-ensure)
+  (clojure-mode-hook . 'eglot-ensure)
+  (latex-mode-hook . 'eglot-ensure)
+  (lua-mode-hook . 'eglot-ensure)
+  (markdown-mode-hook . 'eglot-ensure)
+  (python-mode-hook . 'eglot-ensure)
+  (rust-mode-hook . 'eglot-ensure)
+  (ruby-mode-hook . 'eglot-ensure)
+  (sql-mode-hook . 'eglot-ensure)
+  (typescript-mode-hook . 'eglot-ensure)
+  (html-mode-hook . 'eglot-ensure)
+  (web-mode-hook . 'eglot-ensure)
+  (css-mode-hook . 'eglot-ensure)
+  (json-mode-hook . 'eglot-ensure)
+  (yaml-mode-hook . 'eglot-ensure)
+  (terraform-mode-hook . 'eglot-ensure))
 
 ;; Whitespace
 (use-package whitespace
