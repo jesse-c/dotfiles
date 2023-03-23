62319da540b27522865fe9dd4e65549427dc43ee
Jesse Claven <jesse@duffel.com>
2023-03-23 11:32:35 +0000

    neovim: Update nvim-tree setup

--- home/private_dot_config/nvim/lua/user/file_system.lua
+++ home/private_dot_config/nvim/lua/user/file_system.lua
@@ -1,6 +1,22 @@
 local ok, nvim_tree = pcall(require, "nvim-tree")
 if not ok then return end
 
-nvim_tree.setup()
-
-vim.g.nvim_tree_width = 60
+nvim_tree.setup({
+	open_on_setup = true,
+	view = {
+		width = 60,
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = false,
+		git_clean = false,
+	},
+  git = {
+    ignore = false,
+  },
+  modified = {
+    enable = true,
+  },
+})
--- home/private_dot_config/nvim/lua/user/plugins/file_system.lua
+++ home/private_dot_config/nvim/lua/user/plugins/file_system.lua
@@ -1,6 +1,10 @@
 return {
-    -- Helpers for UNIX
-    "tpope/vim-eunuch", -- Changes Vim working directory to project root
-    "airblade/vim-rooter",
-    {"kyazdani42/nvim-tree.lua", dependencies = "kyazdani42/nvim-web-devicons"}
+	-- Helpers for UNIX
+	"tpope/vim-eunuch",
+	-- Changes Vim working directory to project root
+	"airblade/vim-rooter",
+	{
+		"kyazdani42/nvim-tree.lua",
+		dependencies = "kyazdani42/nvim-web-devicons",
+	},
 }
