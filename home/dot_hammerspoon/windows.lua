e6ce86db166dd88a54ab946f4242285c549af651
Jesse Claven <jesse.claven@me.com>
2023-04-05 21:17:45 +0100

    hammerspoon: Format and add centre 75% window

--- home/dot_hammerspoon/windows.lua
+++ home/dot_hammerspoon/windows.lua
@@ -1,15 +1,43 @@
 -- Window management -----------------------------------------------------------
--- TODO Replace Rectangle
+-- TODO Replace Rectangle to reduce third-party tool count
 
 require("hs.window")
 hs.window.animationDuration = 0
 
 -- Window Switcher
-switcher = hs.window.switcher.new(
-  hs.window.filter.new():setCurrentSpace(true):setDefaultFilter()
-)
+switcher = hs.window.switcher.new(hs.window.filter.new():setCurrentSpace(true):setDefaultFilter())
 switcher.ui.showThumbnails = false
 
-hs.hotkey.bind('alt', 'tab', 'Next window', function() switcher:next() end)
-hs.hotkey.bind('alt-shift', 'tab', 'Prev window',function() switcher:previous() end)
+hs.hotkey.bind("alt", "tab", "Next window", function()
+	switcher:next()
+end)
+hs.hotkey.bind("alt-shift", "tab", "Prev window", function()
+	switcher:previous()
+end)
 
+-- Set the percentage of screen to use
+local percentOfScreen = 0.75
+
+-- Function to center a window on the screen
+function centerWindow(window)
+	local screen = window:screen()
+	local frame = screen:frame()
+	local size = {
+		w = frame.w * percentOfScreen,
+		h = frame.h * percentOfScreen,
+	}
+	local origin = {
+		x = frame.x + ((frame.w - size.w) / 2),
+		y = frame.y + ((frame.h - size.h) / 2),
+	}
+	window:setSize(size)
+	window:setTopLeft(origin)
+end
+
+-- Bind a hotkey to centre the frontmost window
+hs.hotkey.bind({ "cmd", "alt" }, "v", function()
+	local window = hs.window.frontmostWindow()
+	if window then
+		centerWindow(window)
+	end
+end)
