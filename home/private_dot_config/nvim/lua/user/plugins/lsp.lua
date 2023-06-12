a67420b80338ac80f3ea3db3d7f505aa75a7906f
Jesse Claven <jesse@duffel.com>
2023-06-12 11:17:22 +0100

    chore(neovim): Set legacy branch

--- home/private_dot_config/nvim/lua/user/plugins/lsp.lua
+++ home/private_dot_config/nvim/lua/user/plugins/lsp.lua
@@ -55,6 +55,7 @@ return {
     -- Standalone UI for nvim-lsp progress. Eye candy for the impatient.
     {
         "j-hui/fidget.nvim",
+        branch = "legacy",
         dependencies = "neovim/nvim-lspconfig",
         config = function() require("fidget").setup() end
     }
