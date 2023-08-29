0015c1a6cc3a73bc215508aeb4dc0113b74985ab
Jesse Claven <jesse.claven@seldon.io>
2023-08-29 11:08:20 +0100

    feat(neovim): Setup Go dev

--- home/dot_config/nvim/lua/user/plugins/languages.lua
+++ home/dot_config/nvim/lua/user/plugins/languages.lua
@@ -12,6 +12,18 @@ return {
 	"lervag/vimtex",
 	-- plist
 	"darfink/vim-plist",
+	-- Go
+	{
+		"ray-x/go.nvim",
+		dependencies = {
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		event = { "CmdlineEnter" },
+		ft = { "go", "gomod" },
+		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+	},
 	-- Elixir
 	{
 		"elixir-editors/vim-elixir",
--- home/dot_config/nvim/lua/user/plugins/test.lua
+++ home/dot_config/nvim/lua/user/plugins/test.lua
@@ -1,20 +1,38 @@
 return {
-    -- An extensible framework for interacting with tests within NeoVim.
-    {
-        "rcarriga/neotest",
-        dependencies = {
-            "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter",
-            "antoinemadec/FixCursorHold.nvim", -- Adapters
-            "jfpedroza/neotest-elixir", "rouge8/neotest-rust",
-            "nvim-neotest/neotest-python"
-        },
-        config = function()
-            require("neotest").setup({
-                adapters = {
-                    require("neotest-elixir"), require("neotest-rust"),
-                    require("neotest-python")
-                }
-            })
-        end
-    }
+	-- An extensible framework for interacting with tests within NeoVim.
+	{
+		"rcarriga/neotest",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"antoinemadec/FixCursorHold.nvim",
+			-- Adapters
+			"jfpedroza/neotest-elixir",
+			"rouge8/neotest-rust",
+			"nvim-neotest/neotest-python",
+			"nvim-neotest/neotest-go",
+		},
+		config = function()
+			-- get neotest namespace (api call creates or returns namespace)
+			local neotest_ns = vim.api.nvim_create_namespace("neotest")
+			vim.diagnostic.config({
+				virtual_text = {
+					format = function(diagnostic)
+						local message =
+							diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+						return message
+					end,
+				},
+			}, neotest_ns)
+
+			require("neotest").setup({
+				adapters = {
+					require("neotest-elixir"),
+					require("neotest-rust"),
+					require("neotest-python"),
+					require("neotest-go"),
+				},
+			})
+		end,
+	},
 }
