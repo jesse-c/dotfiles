ab1f1bc6e2f4ed6134054b322e313f432c6b7776
Jesse Claven <jesse.claven@me.com>
2023-10-20 21:02:12 +0100

    feat(wezterm): Add tmux keybindings
    Uses auto-generation from Kitty config.

---
+++ home/dot_config/wezterm/keyboard.lua
@@ -0,0 +1,29 @@
+local wezterm = require("wezterm")
+
+local M = {}
+
+M.keys = {
+	{ key = "h", mods = "SUPER", action = wezterm.action({ SendString = "\x02\x68" }) },
+	{ key = "j", mods = "SUPER", action = wezterm.action({ SendString = "\x02\x6a" }) },
+	{ key = "k", mods = "SUPER", action = wezterm.action({ SendString = "\x02\x6b" }) },
+	{ key = "l", mods = "SUPER", action = wezterm.action({ SendString = "\x02\x6c" }) },
+	{ key = "t", mods = "SUPER", action = wezterm.action({ SendString = "\x02\x63" }) },
+	{ key = "w", mods = "SUPER", action = wezterm.action({ SendString = "\x02\x78" }) },
+	{ key = "d", mods = "SUPER", action = wezterm.action({ SendString = "\x02\x22" }) },
+	{ key = "d", mods = "SUPER|SHIFT", action = wezterm.action({ SendString = "\x02\x25" }) },
+	{ key = "h", mods = "SUPER|SHIFT", action = wezterm.action({ SendString = "\x02\x70" }) },
+	{ key = "l", mods = "SUPER|SHIFT", action = wezterm.action({ SendString = "\x02\x6e" }) },
+	{ key = "r", mods = "SUPER", action = wezterm.action({ SendString = "\x02\x2c" }) },
+	{ key = "0", mods = "SUPER", action = wezterm.action({ SendString = "\x02\x27" }) },
+	{ key = "1", mods = "SUPER", action = wezterm.action({ SendString = "\x02\x31" }) },
+	{ key = "2", mods = "SUPER", action = wezterm.action({ SendString = "\x02\x32" }) },
+	{ key = "3", mods = "SUPER", action = wezterm.action({ SendString = "\x02\x33" }) },
+	{ key = "4", mods = "SUPER", action = wezterm.action({ SendString = "\x02\x34" }) },
+	{ key = "5", mods = "SUPER", action = wezterm.action({ SendString = "\x02\x35" }) },
+	{ key = "6", mods = "SUPER", action = wezterm.action({ SendString = "\x02\x36" }) },
+	{ key = "7", mods = "SUPER", action = wezterm.action({ SendString = "\x02\x37" }) },
+	{ key = "8", mods = "SUPER", action = wezterm.action({ SendString = "\x02\x38" }) },
+	{ key = "9", mods = "SUPER", action = wezterm.action({ SendString = "\x02\x39" }) },
+}
+
+return M
--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 
 -- Start
-local config = {}
+local config = wezterm.config_builder()
 
 -- Typography
 local font = require('font')
@@ -15,6 +15,11 @@ config.enable_tab_bar = appearance.enable_tab_bar
 config.warn_about_missing_glyphs = appearance.warn_about_missing_glyphs
 config.color_scheme = appearance.scheme_for_appearance(wezterm.gui.get_appearance())
 config.window_padding = appearance.window_padding
+
+-- Keyboard
+local keyboard = require('keyboard')
+
+config.keys = keyboard.keys
 
 -- Meta
 require('meta').setup()
