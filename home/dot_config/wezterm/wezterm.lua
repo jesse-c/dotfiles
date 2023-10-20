3290a9f45a1a6010a03b483442de0e83bbf986fb
Jesse Claven <jesse.claven@me.com>
2023-10-20 21:07:39 +0100

    chore(wezterm): Formatting

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -1,15 +1,15 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- Start
 local config = wezterm.config_builder()
 
 -- Typography
-local font = require('font')
+local font = require("font")
 
 config.font_size = font.size
 
 -- Appearance
-local appearance = require('appearance')
+local appearance = require("appearance")
 
 config.enable_tab_bar = appearance.enable_tab_bar
 config.warn_about_missing_glyphs = appearance.warn_about_missing_glyphs
@@ -17,12 +17,12 @@ config.color_scheme = appearance.scheme_for_appearance(wezterm.gui.get_appearanc
 config.window_padding = appearance.window_padding
 
 -- Keyboard
-local keyboard = require('keyboard')
+local keyboard = require("keyboard")
 
 config.keys = keyboard.keys
 
 -- Meta
-require('meta').setup()
+require("meta").setup()
 
 -- End
 return config
