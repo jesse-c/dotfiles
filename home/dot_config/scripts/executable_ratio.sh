a86f1decdb970852652120b094eea96c9ace66ad
Jesse Claven <jesse.claven@seldon.io>
2023-11-16 11:00:59 +0000

    fix(sway): Handle multiple displays for ratio resizing

--- home/dot_config/scripts/executable_ratio.sh
+++ home/dot_config/scripts/executable_ratio.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Get the screen width
-screen_width=$(swaymsg -r -t get_outputs | jq '.[] | select(.active == true) | .rect.width')
+screen_width=$(swaymsg -r -t get_outputs | jq '.[] | select(.active == true) | .rect.width' | head -n 1)
 
 # Calculate the new width for the window (1/3 of the screen width)
 new_width=$((screen_width / $1))
