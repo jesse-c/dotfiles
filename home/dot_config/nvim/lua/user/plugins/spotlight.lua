bc10c225444f030f97f29e6fb3a152788ea8cbd1
Jesse Claven <jesse.claven@me.com>
2024-01-29 21:30:58 +0000

    refactor(neovim): Restore more functionality

--- home/dot_config/nvim/lua/user/plugins/spotlight.lua
+++ home/dot_config/nvim/lua/user/plugins/spotlight.lua
@@ -7,6 +7,29 @@ return {
     event = { "BufRead" },
     keys = {
       { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
     },
+    opts = {
+      extensions = {
+        fzf = {
+          fuzzy = true, -- false will only do exact matching
+          override_generic_sorter = true, -- override the generic sorter
+          override_file_sorter = true, -- override the file sorter
+          case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+        },
+      },
+    },
+    config = function(_, opts)
+      local telescope = require("telescope")
+
+      telescope.setup(opts)
+      telescope.load_extension("fzf")
+    end,
+  },
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    event = { "BufRead" },
   },
 }
