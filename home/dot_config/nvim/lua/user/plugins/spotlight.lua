d0e24dfc5816a21eaa9ea0b8da8b19a44b55d6de
Jesse Claven <jesse.claven@me.com>
2024-04-06 21:53:16 +0100

    feat(neovim): Add AI plugin

--- home/dot_config/nvim/lua/user/plugins/spotlight.lua
+++ home/dot_config/nvim/lua/user/plugins/spotlight.lua
@@ -1,38 +1,43 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope-fzf-native.nvim",
-    },
-    cmd = "Telescope",
-    event = { "BufRead" },
-    keys = {
-      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
-      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
-      { "<leader>b", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-    },
-    config = function()
-      local telescope = require("telescope")
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-fzf-native.nvim",
+		},
+		cmd = "Telescope",
+		event = { "BufRead" },
+		keys = {
+			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+			{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+			{ "<leader>b", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+		},
+		config = function()
+			local telescope = require("telescope")
 
-      telescope.setup({
-        extensions = {
-          fzf = {
-            fuzzy = true, -- false will only do exact matching
-            override_generic_sorter = true, -- override the generic sorter
-            override_file_sorter = true, -- override the file sorter
-            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-          },
-        },
-      })
+			telescope.setup({
+				extensions = {
+					fzf = {
+						fuzzy = true, -- false will only do exact matching
+						override_generic_sorter = true, -- override the generic sorter
+						override_file_sorter = true, -- override the file sorter
+						case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+					},
+				},
+			})
 
-      telescope.load_extension("fzf")
-    end,
-  },
-  {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
-    event = { "BufRead" },
-  },
+			telescope.load_extension("fzf")
+		end,
+	},
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
+		event = { "BufRead" },
+	},
+	{
+		"ibhagwan/fzf-lua",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = true,
+	},
 }
