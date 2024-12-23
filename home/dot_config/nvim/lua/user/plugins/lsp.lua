e0b00f3a61b8dae4e4514cd38b52e2fc1ccfe272
Jesse Claven <jesse.claven@me.com>
2024-12-23 22:34:01 +0000

    feat(neovim): Use latest blink.cmp config

--- home/dot_config/nvim/lua/user/plugins/lsp.lua
+++ home/dot_config/nvim/lua/user/plugins/lsp.lua
@@ -1,30 +1,31 @@
 return {
-  -- LSP servers and clients communicate what features they support through "capabilities".
-  --  By default, Neovim support a subset of the LSP specification.
-  --  With blink.cmp, Neovim has *more* capabilities which are communicated to the LSP servers.
-  --  Explanation from TJ: https://youtu.be/m8C0Cq9Uv9o?t=1275
-  --
-  -- This can vary by config, but in general for nvim-lspconfig:
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = { "saghen/blink.cmp" },
-    config = function(_, opts)
-      local lspconfig = require("lspconfig")
-      for server, config in pairs(opts.servers or {}) do
-        config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-        lspconfig[server].setup(config)
-      end
-    end,
-  },
-  {
-    "j-hui/fidget.nvim",
-    opts = {},
-    event = { "BufRead" },
-  },
-  {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons", "neovim/nvim-lspconfig" },
-    opts = {},
-    event = { "BufRead" },
-  },
+	-- LSP servers and clients communicate what features they support through "capabilities".
+	--  By default, Neovim support a subset of the LSP specification.
+	--  With blink.cmp, Neovim has *more* capabilities which are communicated to the LSP servers.
+	--  Explanation from TJ: https://youtu.be/m8C0Cq9Uv9o?t=1275
+	--
+	-- This can vary by config, but in general for nvim-lspconfig:
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "saghen/blink.cmp" },
+		config = function(_, opts)
+			local lspconfig = require("lspconfig")
+
+			for server, config in pairs(opts.servers or {}) do
+				config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+				lspconfig[server].setup(config)
+			end
+		end,
+	},
+	{
+		"j-hui/fidget.nvim",
+		opts = {},
+		event = { "BufRead" },
+	},
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons", "neovim/nvim-lspconfig" },
+		opts = {},
+		event = { "BufRead" },
+	},
 }
