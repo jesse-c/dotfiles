cb22b57daa40dba485e7c0703241b65a099104f9
Jesse Claven <jesse.claven@me.com>
2024-12-24 13:03:33 +0000

    feat(emacs): Clean-up AI package

--- home/dot_config/emacs/user/ai.el
+++ home/dot_config/emacs/user/ai.el
@@ -1,16 +1,5 @@
 ;;; ai.el --- -*- lexical-binding: t; -*-
 
-(use-package copilot
-  :disabled
-  :after jsonrpc
-  :ensure
-  (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
-  :hook
-  (prog-mode-hook . copilot-mode)
-  (text-mode-hook . copilot-mode)
-  :custom
-  (copilot-enable-predicates '(evil-insert-state-p)))
-
 (require 'auth-source)
 
 (defun my/get-password (hostname username)
@@ -24,17 +13,6 @@
     (if match
         (funcall (plist-get (car match) :secret))
       (error "No password found for %s@%s in .authinfo" username hostname))))
-
-(use-package chatgpt-arcana
-  :disabled
-  :after all-the-icons
-  :ensure
-  (:host github :repo "CarlQLange/ChatGPT-Arcana.el" :files ("*.el"))
-  :custom
-  (chatgpt-arcana-api-key (my/get-password "api.openai.com" "me"))
-  :config
-  (add-to-list 'all-the-icons-mode-icon-alist
-               '(chatgpt-arcana-chat-mode all-the-icons-octicon "comment-discussion" :height 1.0 :v-adjust -0.1 :face all-the-icons-purple)))
 
 (use-package gptel
   :after transient
@@ -50,16 +28,17 @@
   :config
   (setq gptel-backend (gptel-make-anthropic "Claude" :stream t :key (my/get-password "anthropic.com" "me"))))
 
-(use-package ellama
-  :disabled
-  :ensure
-  (:host github :repo "s-kostyaev/ellama"))
-
 (use-package org-ai
-  :disabled)
-
-(use-package llm
-  :disabled)
+  :ensure t
+  :commands (org-ai-mode
+             org-ai-global-mode)
+  :hook
+  (org-mode . org-ai-mode)
+  :init
+  (org-ai-global-mode) ; installs global keybindings on C-c M-a
+  :custom
+  (org-ai-openai-api-token (my/get-password "api.openai.com" "me"))
+  (org-ai-default-chat-model "gpt-4-turbo"))
 
 (use-package aider
   :after transient
