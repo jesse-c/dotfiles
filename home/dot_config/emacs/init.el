43e7a3259b442246e8b1872d3cdbe6282a6fca93
Jesse Claven <jesse.claven@me.com>
2025-08-03 16:27:15 +0100

    feat(emacs): Add formatter for Fish

--- home/dot_config/emacs/init.el
+++ home/dot_config/emacs/init.el
@@ -2421,7 +2421,9 @@ are defining or executing a macro."
   :config
   (add-to-list 'apheleia-mode-alist '(json-ts-mode . denofmt-ts))
   (add-to-list 'apheleia-mode-alist '(typescript-ts-mode . denofmt-ts))
-  (add-to-list 'apheleia-mode-alist '(python-base-mode . ruff)))
+  (add-to-list 'apheleia-mode-alist '(python-base-mode . ruff))
+  (add-to-list 'apheleia-formatters '(fish-indent . ("fish_indent")))
+  (add-to-list 'apheleia-mode-alist '(fish-mode . fish-indent)))
 
 ;; Spelling & Grammar
 
