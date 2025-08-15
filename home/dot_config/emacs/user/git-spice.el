7951308024c163aecac43372b561ba4b810d5a65
Jesse Claven <jesse.claven@me.com>
2025-08-15 20:25:49 +0100

    feat(emacs): Display git-spice as tree

--- home/dot_config/emacs/user/git-spice.el
+++ home/dot_config/emacs/user/git-spice.el
@@ -3,6 +3,75 @@
 (remove-hook 'magit-status-sections-hook 'my-magit-git-spice-working)
 (remove-hook 'magit-status-sections-hook 'my-magit-insert-dummy-section)
 
+(defun my-git-spice-insert-branch-tree (branch branches-by-index current-branch prefix is-last)
+  "Insert a branch and its children in tree format."
+  (let* ((name (alist-get 'Name branch))
+         (change-id (alist-get 'ChangeID branch))
+         (pr-number (when (and change-id (not (eq change-id :null))) 
+                      (alist-get 'number change-id)))
+         (needs-push (eq (alist-get 'NeedsPush branch) :true))
+         (needs-restack (eq (alist-get 'NeedsRestack branch) :true))
+         (commits (let ((c (alist-get 'Commits branch)))
+                    (if (eq c :null) nil c)))
+         (aboves (let ((a (alist-get 'Aboves branch)))
+                   (if (eq a :null) nil a)))
+         (is-current (string= name current-branch))
+         (branch-symbol (if is-current "■" "□"))
+         (current-marker (if is-current " ◀" ""))
+         (tree-char (if is-last "┗━" "┣━"))
+         (status-flags ""))
+    
+    ;; Build status flags
+    (when needs-restack (setq status-flags (concat status-flags " (needs restack)")))
+    (when needs-push (setq status-flags (concat status-flags " (needs push)")))
+    
+    ;; Format branch line
+    (let ((branch-line (format "%s%s%s %s%s%s%s\n"
+                              prefix
+                              tree-char
+                              branch-symbol
+                              name
+                              (if pr-number (format " (#%d)" pr-number) "")
+                              status-flags
+                              current-marker)))
+      
+      ;; Insert clickable branch line
+      (if is-current
+          (insert (propertize branch-line 'face 'magit-branch-current))
+        (magit-insert-section (branch name t)
+          (insert (propertize branch-line
+                             'face 'magit-branch-local
+                             'mouse-face 'highlight
+                             'keymap (let ((map (make-sparse-keymap)))
+                                       (define-key map (kbd "RET") 
+                                         `(lambda () (interactive) (magit-checkout ,name)))
+                                       (define-key map [mouse-1]
+                                         `(lambda () (interactive) (magit-checkout ,name)))
+                                       map))))))
+    
+    ;; Insert commits for this branch
+    (when commits
+      (let ((commit-prefix (concat prefix (if is-last "   " "┃   "))))
+        (dolist (commit commits)
+          (let ((short-hash (alist-get 'ShortHash commit))
+                (subject (alist-get 'Subject commit)))
+            (insert (format "%s%s %s\n" 
+                           commit-prefix
+                           (propertize short-hash 'face 'magit-hash)
+                           subject))))))
+    
+    ;; Insert children branches
+    (when aboves
+      (let ((child-prefix (concat prefix (if is-last "   " "┃")))
+            (child-count (length aboves)))
+        (dotimes (i child-count)
+          (let* ((child-index (nth i aboves))
+                 (child-branch (gethash child-index branches-by-index))
+                 (is-last-child (= i (1- child-count))))
+            (when child-branch
+              (my-git-spice-insert-branch-tree child-branch branches-by-index current-branch 
+                                             child-prefix is-last-child))))))))
+
 (defun my-magit-insert-dummy-section ()
   "Insert a dummy section in Magit status buffer."
   (magit-insert-section (git-spice nil t)
@@ -17,25 +86,19 @@
                             (json-parse-string output :object-type 'alist :array-type 'list)
                           (error nil))))
         (if json-data
-            (progn
-              (insert "Branches:\n")
+            (let ((branches-by-index (make-hash-table :test 'equal))
+                  (main-branch nil))
+              ;; Index branches by their index for quick lookup
               (dolist (branch json-data)
-                (let ((name (alist-get 'Name branch)))
-                  (when name
-                    (if (string= name current-branch)
-                        (insert (format "* %s\n" (propertize name 'face 'magit-branch-current)))
-                      (insert (format "  %s\n" 
-                                      (propertize name
-                                                  'face 'magit-branch-local
-                                                  'keymap (let ((map (make-sparse-keymap)))
-                                                            (define-key map [mouse-1] 
-                                                              `(lambda () (interactive) 
-                                                                 (magit-checkout ,name)))
-                                                            (define-key map (kbd "RET")
-                                                              `(lambda () (interactive)
-                                                                 (magit-checkout ,name)))
-                                                            map)
-                                                  'mouse-face 'highlight))))))))
+                (let ((index (alist-get 'Index branch))
+                      (name (alist-get 'Name branch)))
+                  (puthash index branch branches-by-index)
+                  (when (string= name "main")
+                    (setq main-branch branch))))
+              
+              ;; Build and display tree starting from main
+              (when main-branch
+                (my-git-spice-insert-branch-tree main-branch branches-by-index current-branch "" t)))))
           (insert (format "Failed to parse JSON:\n%s\n" output)))))))
 
 (add-hook 'magit-status-sections-hook 'my-magit-insert-dummy-section t)
