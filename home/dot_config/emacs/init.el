d211bc24a612407b02a3b91b983e06f37c703085
Jesse Claven <jesse.claven@me.com>
2025-08-27 23:22:36 +0100

    feat(emacs): Add compilation improvement

--- home/dot_config/emacs/init.el
+++ home/dot_config/emacs/init.el
@@ -18,6 +18,22 @@
 
 (add-to-list 'load-path (expand-file-name "user/" user-emacs-directory))
 
+(use-package compile-angel
+  :ensure t
+  :demand t
+  :disabled
+  :config
+  ;; Set `compile-angel-verbose' to nil to disable compile-angel messages.
+  ;; (When set to nil, compile-angel won't show which file is being compiled.)
+  (setq compile-angel-verbose t)
+
+  ;; Uncomment the line below to compile automatically when an Elisp file is saved
+  ;; (add-hook 'emacs-lisp-mode-hook #'compile-angel-on-save-local-mode)
+
+  ;; A global mode that compiles .el files before they are loaded
+  ;; using `load' or `require'.
+  (compile-angel-on-load-mode 1))
+
 (use-package emacs
   :custom
   ;; Support opening new minibuffers from inside existing minibuffers.
