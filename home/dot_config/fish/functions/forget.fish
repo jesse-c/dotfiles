b7583c205340ed0034227c9d9b8a24a58f17288e
Jesse Claven <jesse.claven@me.com>
2025-08-27 23:22:59 +0100

    fix(fish): Ignore empty prompt with forget function

--- home/dot_config/fish/functions/forget.fish
+++ home/dot_config/fish/functions/forget.fish
@@ -3,6 +3,11 @@
 # https://www.milanvit.net/post/my-ultimate-shell-setup-with-fish-shell-and-tmux/
 function forget -d "Remove a command from shell history"
     set -l cmd (commandline | string collect)
+    # Skip if command line is empty
+    if test -z "$cmd"
+        commandline -f repaint
+        return
+    end
     printf "\nDo you want to forget '%s'? [Y/n]\n" $cmd
     switch (read | tr A-Z a-z)
         case n no
