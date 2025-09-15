9b1d429bd31d41d7c736b84f5a20cfd08c4b6e56
Jesse Claven <jesse.claven@me.com>
2025-09-15 23:05:39 +0100

    fix(fish): Remove unneeded if-else branch

--- home/dot_config/fish/functions/pyr.fish
+++ home/dot_config/fish/functions/pyr.fish
@@ -3,14 +3,16 @@ function pyr -d "Run within Python tooling"
         poetry run $argv
     else if test -f pyproject.toml -a -f uv.lock
         uv run $argv
-    else if test -f pyproject.toml
-        poetry run $argv
     else
         echo "No pyproject.toml found - neither poetry nor uv project detected"
         return 1
     end
 end
 
-function pyrp
+function pyrp -d "Run within Python interpreator in Python tooling"
     pyr python $argv
+end
+
+function pyri -d "Run IPython interpreator in Python tooling"
+    pyr ipython
 end
