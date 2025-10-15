910b72800f1356f87c2c22dd64fab63d714d7300
Jesse Claven <jesse.claven@me.com>
2025-10-15 12:38:53 +0100

    fix(emacs): Use user path for Mise setup

--- home/dot_config/emacs/init.el
+++ home/dot_config/emacs/init.el
@@ -257,8 +257,9 @@ PACKAGES should be a list of package names as symbols."
 ;; https://mise.jdx.dev/ide-integration.html
 ;; CLI tools installed by Mise
 ;; See: https://www.emacswiki.org/emacs/ExecPath
-(setenv "PATH" (concat (getenv "PATH") ":/home/user/.local/share/mise/shims"))
-(setq exec-path (append exec-path '("/home/user/.local/share/mise/shims")))
+(let ((mise-shims (expand-file-name "~/.local/share/mise/shims")))
+  (setenv "PATH" (concat (getenv "PATH") ":" mise-shims))
+  (setq exec-path (append exec-path (list mise-shims))))
 
 ;;; Project
 
