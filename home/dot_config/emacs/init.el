8318467665f21a7fd8991f41638a673444eff82e
Jesse Claven <jesse.claven@me.com>
2026-03-20 13:16:57 +0000

    fix(emacs): Get rid of unused :config

--- home/dot_config/emacs/init.el
+++ home/dot_config/emacs/init.el
@@ -1765,21 +1765,16 @@ are defining or executing a macro."
   :after corfu
   ;; Bind prefix keymap providing all Cape commands under a mnemonic key.
   ;; Press C-c p ? to for help.
-  :bind ("C-c p" . cape-prefix-map) ;; Alternative key: M-<tab>, M-p, M-+
-  ;; Alternatively bind Cape commands individually.
-  ;; :bind (("C-c p d" . cape-dabbrev)
-  ;;        ("C-c p h" . cape-history)
-  ;;        ("C-c p f" . cape-file)
-  ;;        ...)
-  :config
-  ;; Add to the global default value of `completion-at-point-functions' which is
-  ;; used by `completion-at-point'.  The order of the functions matters, the
-  ;; first function returning a result wins.  Note that the list of buffer-local
-  ;; completion functions takes precedence over the global list.
-  ;; Consider:
-  ;; https://github.com/minad/corfu/wiki#continuously-update-the-candidates
-  (advice-add 'eglot-completion-at-point :around #'cape-wrap-buster))
-
+  :bind ("C-c p" . cape-prefix-map)) ;; Alternative key: M-<tab>, M-p, M-+
+;; Alternatively bind Cape commands individually.
+;; :bind (("C-c p d" . cape-dabbrev)
+;;        ("C-c p h" . cape-history)
+;;        ("C-c p f" . cape-file)
+;;        ...)
+;; Add to the global default value of `completion-at-point-functions' which is
+;; used by `completion-at-point'.  The order of the functions matters, the
+;; first function returning a result wins.  Note that the list of buffer-local
+;; completion functions takes precedence over the global list.
 
 (after-packages (cape tempel corfu)
   ;; Global completion functions
